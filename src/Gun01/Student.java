47183da99ff996a62af9d4aebcdd575c8a0cdc66
yurtbilen <t_yurtbilen@hotmail.com>
2023-01-26 19:32:50 +0300

    task 2 yapıldı.

--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -3,5 +3,6 @@ package Gun01;
 public class Student {
     public static void main(String[] args) {
         System.out.println("merhaba 1");
+        System.out.println("task 2 yapıldı");
     }
 }
